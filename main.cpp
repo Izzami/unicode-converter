98810ee952d69c420ef926e69082848ef8ae348e
Izzami <39417344+Izzami@users.noreply.github.com>
2022-12-01 19:39:18 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cstdlib>
+#include<cstring>
+using namespace std;
+#define m 1024
+
+
+
+int main()
+{
+
+
+	string frases,linea,conve;
+	char frasec[m];
+	cout << "Input: ";
+	cin >> frases;
+	strcpy(frasec, frases.c_str());
+	ifstream tabella;
+	tabella.open("table.txt", ios::in);
+	if (!tabella)
+		exit(1);
+	for (int i = 0; i < frases.length(); i++)
+	{
+
+		int current = 0;
+		while (getline(tabella, linea))
+		{
+			if (current == (int)frasec[i])
+			{
+				cout<< linea.substr(0, 6)<<" ";
+
+				break;
+			}
+			current++;
+		}
+		tabella.clear();
+		tabella.seekg(0);
+		
+	}
+	cout << endl;
+	#ifdef _WIN32
+		system("pause");
+	#endif
+exit (0);
+}
